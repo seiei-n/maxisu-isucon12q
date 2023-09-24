1f88cc446c4d7dc79f6cfdf49caef58d4892b629
seiei-n <solehamugoiyo@gmail.com>
2023-09-24 20:08:58 +0900

    fix index

--- sql/tenant/20_index.sql
+++ sql/tenant/20_index.sql
@@ -1,4 +1,3 @@
 CREATE INDEX idx_competition_tenant_id ON competition (tenant_id, id, created_at, finished_at);
 CREATE INDEX idx_player ON player (tenant_id, id, display_name, is_disqualified, created_at, updated_at);
 CREATE INDEX idx_player_score ON player_score (tenant_id, player_id, competition_id, score, row_num, created_at, updated_at);
-```
