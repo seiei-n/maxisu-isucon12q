dbe6f8087744ec848533728f5260f3ad993c87fc
seiei-n <solehamugoiyo@gmail.com>
2023-09-24 18:48:02 +0900

    fix index

--- sql/init.sh
+++ sql/init.sh
@@ -14,7 +14,7 @@ mysql -u"$ISUCON_DB_USER" \
 		-p"$ISUCON_DB_PASSWORD" \
 		--host "$ISUCON_DB_HOST" \
 		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME" < init.sql
+		"$ISUCON_DB_NAME" < init.sql &
 
 # SQLiteのデータベースを初期化
 rm -f ../tenant_db/*.db
--- sql/init.sql
+++ sql/init.sql
@@ -5,4 +5,4 @@ ALTER TABLE id_generator AUTO_INCREMENT=2678400000;
 
 -- CREATE INDEX visit_history_id_idx ON visit_history ( `competition_id` , `created_at` , `tenant_id` )
 ALTER TABLE visit_history DROP INDEX visit_history_id_idx;
-ALTER TABLE visit_history ADD INDEX visit_history_id_idx ( `competition_id`  , `tenant_id` );
+ALTER TABLE visit_history ADD INDEX visit_history_id_idx ( `player_id` , `competition_id` , `created_at` , `tenant_id` , `updated_at` );
