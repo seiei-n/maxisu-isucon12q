04c81654cd91a4a3cfbf528efb8c79532029bf03
seiei-n <solehamugoiyo@gmail.com>
2023-09-24 20:07:40 +0900

    dix

--- sql/tenant/20_index.sql
+++ sql/tenant/20_index.sql
@@ -1,3 +1,4 @@
-CREATE INDEX idx_competition_tenant_id ON competition (tenant_id, id);
-CREATE INDEX idx_player ON player (tenant_id, id, display_name, is_disqualified);
-CREATE INDEX idx_player_score ON player_score (tenant_id, player_id, competition_id, score, row_num);
+CREATE INDEX idx_competition_tenant_id ON competition (tenant_id, id, created_at, finished_at);
+CREATE INDEX idx_player ON player (tenant_id, id, display_name, is_disqualified, created_at, updated_at);
+CREATE INDEX idx_player_score ON player_score (tenant_id, player_id, competition_id, score, row_num, created_at, updated_at);
+```
